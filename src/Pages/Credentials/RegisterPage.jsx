628f2ba91313884ead33e2325525f3c30d42dd38
Gihoon Kim <gkim61@myseneca.ca>
2024-01-14 19:55:20 -0500

    User Add (Not verificated yet)

--- src/Pages/Credentials/RegisterPage.jsx
+++ src/Pages/Credentials/RegisterPage.jsx
@@ -2,7 +2,7 @@ import { Button, Container, Paper, TextField } from "@mui/material";
 import axios from "axios";
 import React, { useState } from "react";
 import PasswordChecklist from "react-password-checklist";
-import { Form, json } from "react-router-dom";
+import { json } from "react-router-dom";
 import validator from "validator";
 import styles from "./RegisterPage.module.css";
 import { ConfirmDialog } from "./Components/ConfirmDialog";
@@ -16,6 +16,7 @@ const RegisterPage = () => {
   const [passwordValidated, setPasswordValidated] = useState(false);
   const [isEmailConfirmDialogOpen, setIsEmailConfirmDialogOpen] =
     useState(false);
+  const [userName, setUserName] = useState("");
 
   const onEmailChange = (event) => {
     event.preventDefault();
@@ -33,6 +34,11 @@ const RegisterPage = () => {
   const onPasswordCheckerChange = (event) => {
     event.preventDefault();
     setPasswordChecker(event.target.value);
+  };
+
+  const onUserNameChange = (event) => {
+    event.preventDefault();
+    setUserName(event.target.value);
   };
 
   const onEmailCheckClickEvent = async (event) => {
@@ -70,94 +76,128 @@ const RegisterPage = () => {
     }
   };
 
-  // const onPasswordChange = (event) => {
-  //   setPassword(event.target.value);
-  // };
+  const onRegister = async () => {
+    const response = await axios.post(
+      "http://localhost:8080/api/userAuth/signup",
+      {
+        name: userName,
+        email: email,
+        password: password,
+      }
+    );
 
-  // const onPasswordCheckerChange = (event) => {
-  //   setPasswordChecker(event.target.value);
-  // };
+    if (response.statusText !== "OK") {
+      throw json(
+        { message: "Could not fetch events." },
+        {
+          status: 500,
+        }
+      );
+    } else {
+      console.log(response);
+    }
+  };
 
   return (
-    <Container sx={{ marginTop: "30px" }}>
-      <Paper elevation={16} style={{ padding: "30px" }}>
-        <Form method="POST">
-          <Container
-            sx={{
-              textAlign: "center",
-              marginBottom: "1rem",
-            }}
-          >
-            <h1>Enter your email</h1>
-            <div className={styles.email}>
+    <Container
+      sx={{
+        marginTop: "30px",
+      }}
+    >
+      <Paper
+        elevation={16}
+        style={{
+          padding: "30px",
+        }}
+      >
+        <Container
+          sx={{
+            textAlign: "center",
+            marginBottom: "1rem",
+          }}
+        >
+          <h1>Enter your email</h1>
+          <div className={styles.email}>
+            <TextField
+              id="outlined-basic"
+              label="Email"
+              type="email"
+              variant="outlined"
+              fullWidth
+              value={email}
+              required
+              onChange={onEmailChange}
+              disabled={emailValidated}
+            />
+            <Button
+              disabled={
+                emailValidated ||
+                email.length === 0 ||
+                !validator.isEmail(email)
+              }
+              variant="outlined"
+              onClick={onEmailCheckClickEvent}
+              sx={{
+                marginLeft: "10px",
+                textTransform: "none",
+              }}
+            >
+              Check Email
+            </Button>
+          </div>
+
+          {isEmailConfirmDialogOpen && (
+            <ConfirmDialog
+              email={email}
+              isOpen={isEmailConfirmDialogOpen}
+              emailUsable={emailUsable}
+              handleClose={handleEmailValidateDialogClose}
+            />
+          )}
+        </Container>
+
+        {emailValidated && (
+          <div style={{ display: "block" }}>
+            <Container sx={{ marginBottom: "1rem" }}>
               <TextField
-                id="outlined-basic"
-                label="Email"
-                type="email"
-                variant="outlined"
                 fullWidth
-                value={email}
-                required
-                onChange={onEmailChange}
-                disabled={emailValidated}
-              />
-              <Button
-                disabled={
-                  emailValidated ||
-                  email.length === 0 ||
-                  !validator.isEmail(email)
-                }
                 variant="outlined"
-                onClick={onEmailCheckClickEvent}
-                sx={{
-                  marginLeft: "10px",
-                  textTransform: "none",
-                }}
-              >
-                Check Email
-              </Button>
-            </div>
-
-            {isEmailConfirmDialogOpen && (
-              <ConfirmDialog
-                email={email}
-                isOpen={isEmailConfirmDialogOpen}
-                emailUsable={emailUsable}
-                handleClose={handleEmailValidateDialogClose}
+                label="User Name"
+                value={userName}
+                required
+                onChange={onUserNameChange}
               />
-            )}
-          </Container>
+            </Container>
 
-          {emailValidated && (
-            <div style={{ display: "block" }}>
-              <Container
-                sx={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                }}
-              >
-                <TextField
-                  fullWidth
-                  label="Password"
-                  type="password"
-                  variant="outlined"
-                  value={password}
-                  required
-                  onChange={onPasswordChange}
-                  style={{ marginRight: "50px" }}
-                />
+            <Container
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <TextField
+                fullWidth
+                label="Password"
+                type="password"
+                variant="outlined"
+                value={password}
+                required
+                onChange={onPasswordChange}
+                style={{ marginRight: "50px" }}
+              />
 
-                <TextField
-                  fullWidth
-                  label="Password Check"
-                  type="password"
-                  variant="outlined"
-                  value={passwordChecker}
-                  required
-                  onChange={onPasswordCheckerChange}
-                />
-              </Container>
+              <TextField
+                fullWidth
+                label="Password Check"
+                type="password"
+                variant="outlined"
+                value={passwordChecker}
+                required
+                onChange={onPasswordCheckerChange}
+              />
+            </Container>
 
+            <Container>
               <PasswordChecklist
                 rules={[
                   "minLength",
@@ -174,15 +214,17 @@ const RegisterPage = () => {
                   else setPasswordValidated(false);
                 }}
               />
-            </div>
-          )}
+            </Container>
+          </div>
+        )}
 
-          {passwordValidated && (
-            <div>
-              <button>Register</button>
-            </div>
-          )}
-        </Form>
+        {passwordValidated && userName !== "" && (
+          <div className={styles.register}>
+            <Button variant="outlined" onClick={onRegister}>
+              Register
+            </Button>
+          </div>
+        )}
       </Paper>
     </Container>
   );
